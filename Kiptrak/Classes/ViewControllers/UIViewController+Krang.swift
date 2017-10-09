3070a4a8217df08db10e53d6d739ccc81373f9b8
Elliott Kipper <kip@kipper.me>
2017-10-08 19:10:48 -0700

    Better height

--- Kiptrak/Classes/ViewControllers/UIViewController+Krang.swift
+++ Kiptrak/Classes/ViewControllers/UIViewController+Krang.swift
@@ -12,7 +12,7 @@ import Pulley
 extension UIViewController {
     
     class var defaultPartialRevealDrawerHeight: CGFloat { return 300.0 }
-    class var defaultCollapsedDrawerHeight: CGFloat { return (68.0 + (KrangUtils.Display.typeIsLike == .iphoneX ? 25.0 : 0.0)) }
+    class var defaultCollapsedDrawerHeight: CGFloat { return (82.0 + (KrangUtils.Display.typeIsLike == .iphoneX ? 25.0 : 0.0)) }
     
     func topViewController() -> UIViewController {
         if let presentedViewController = self.presentedViewController {
