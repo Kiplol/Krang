d14ed1d1769613a5eb33d60debbcd1ad28eecbe4
Elliott Kipper <kip@kipper.me>
2018-01-18 13:27:07 -0800

    Remove some whitespace

--- Kiptrak/AppDelegate.swift
+++ Kiptrak/AppDelegate.swift
@@ -23,7 +23,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
     
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         AppDelegate.instance = self;
         FirebaseApp.configure()
