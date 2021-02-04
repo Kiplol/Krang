2e28765b448b15c0fdf4cb114ffbe407bf3cdbf2
Kip Kipper <kip@kipper.me>
2021-02-04 10:14:39 -0800

    Fix season images, I hope.

--- Kiptrak/Classes/Views/Cells/SeasonCollectionViewCell.swift
+++ Kiptrak/Classes/Views/Cells/SeasonCollectionViewCell.swift
@@ -33,31 +33,45 @@ class SeasonCollectionViewCell: UICollectionViewCell, SelfSizingCell {
         self.realmChangeToken?.invalidate()
         self.retrieveImageTask?.cancel()
         self.retrieveImageTask = nil
+        self.imageSeen.isHidden = true
     }
     
     func update(withSeason season: KrangSeason) {
         self.labelTitle.text = season.title
-        
-        self.realmChangeToken?.invalidate()
-        self.retrieveImageTask?.cancel()
-        self.retrieveImageTask = nil
+
         self.imageSeen.isHidden = !(UserPrefs.traktSync && season.hasBeenWatched()) //Must work on getting the entire show/season/episode data from the get-go.
         
         //Use image that's already in there as a placeholder
-        if let posterImageURL = season.posterImageURL {
-            self.imageView.kf.setImage(with: (URL(string: posterImageURL) as? ImageDataProvider), placeholder: self.imageView.image, options: [.transition(.fade(0.2))], progressBlock: nil, completionHandler: nil)
+        if let posterImageURLString = season.posterImageURL, let posterImageURL = URL(string: posterImageURLString) {
+            self.retrieveImageTask = self.imageView.kf.setImage(with: posterImageURL, placeholder: self.imageView.image, options: nil, progressBlock: { progress, max in
+                //
+            }, completionHandler: { result in
+                switch result {
+                case .failure(_):
+                    if let url = season.show?.imagePosterURLs.first?.value {
+                        self.retrieveImageTask = self.imageView.kf.setImage(with: URL(string: url))
+                    }
+                    
+                default:
+                    break
+                }
+            })
         } else if SeasonCollectionViewCell.listenForRealmChanges {
             let query = try! Realm().objects(KrangSeason.self).filter("traktID == %d", season.traktID)
             self.realmChangeToken = query.observe({ change in
-                if query.count > 0 {
-                    if let updatedPosterImageURL = query[0].posterImageURL ?? query[0].show?.imagePosterURL {
-                        self.imageView.kf.setImage(with: (URL(string: updatedPosterImageURL) as? ImageDataProvider), placeholder: self.imageView.image, options: [.transition(.fade(0.2))], progressBlock: nil, completionHandler: nil)
+                switch change {
+                case .update(let results, deletions: _, insertions: _, modifications: _):
+                    if let updatedPosterImageURL = results.first?.posterImageURL ?? query[0].show?.imagePosterURL {
+                        self.retrieveImageTask = self.imageView.kf.setImage(with: (URL(string: updatedPosterImageURL) as? ImageDataProvider), placeholder: self.imageView.image, options: [.transition(.fade(0.2))], progressBlock: nil, completionHandler: nil)
                     }
+                default:
+                    break
                 }
+
             })
         } else {
             if let showPosterImage = season.show?.imagePosterURL {
-                self.imageView.kf.setImage(with: URL(string: showPosterImage))
+                self.retrieveImageTask = self.imageView.kf.setImage(with: URL(string: showPosterImage))
             } else {
                 self.imageView.image = #imageLiteral(resourceName: "poster_placeholder_dark")
             }
